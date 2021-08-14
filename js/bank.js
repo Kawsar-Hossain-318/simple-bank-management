f4bdcefb7501c1451e4ea1f0c2e32f2a0b09be84
Kawsar-Hossain-318 <thekawsarhossain@gmail.com>
2021-08-15 01:02:04 +0600

    login page added

---
+++ js/bank.js
@@ -0,0 +1,18 @@
+document.getElementById('login-btn').addEventListener('click', function(){
+    // user email
+    const userEmailField = document.getElementById('user-email');
+    const userEmail = userEmailField.value;
+
+    // user password
+    const userPasswordField = document.getElementById('user-password');
+    const userPassword = userPasswordField.value;
+
+    // user email and password checking here
+    if(userEmail == 'kawsarhossain@gmail.com' && userPassword == 'secret'){
+        window.location.href="banking.html";
+    }
+    else{
+        const error = document.getElementById('alert-message');
+        error.innerText="Your Email And Password Are Incorrect !";
+    }
+});
